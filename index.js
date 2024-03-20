772bca74bfd87c321dc6e4bf0076031331c90195
ParthibBr <bparthib77@gmail.com>
2024-03-21 00:09:56 +0600

    chef details added

--- index.js
+++ index.js
@@ -23,20 +23,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const menuCollection = client.db("eatingtimeDb").collection("menu");
+    const chefCollection = client.db("eatingtimeDb").collection("chef");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
+    app.get("/chef", async (req, res) => {
+      const result = await chefCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
